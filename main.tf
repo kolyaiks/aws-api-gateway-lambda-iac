7712026e105df9e514007ded6c2dc9a2ccbeaf73
kolyaiks <nikolay.iks@gmail.com>
2021-11-26 00:32:25 +0300

    logging rights added

--- main.tf
+++ main.tf
@@ -27,6 +27,27 @@ resource "aws_iam_role" "lambda-iam" {
 EOF
 }
 
+resource "aws_iam_role_policy" "allow_write_to_cloud_watch" {
+  policy = jsonencode(
+  {
+    Version : "2012-10-17",
+    Statement : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:DescribeLogStreams"
+        ], Resource : [
+        "*"
+      ]
+      }
+    ]
+  })
+  role   = aws_iam_role.lambda-iam.id
+}
+
 resource "aws_lambda_function" "lambda" {
   filename         = "lambda.zip"
   function_name    = "lambda-function"
@@ -48,23 +69,23 @@ resource "aws_apigatewayv2_stage" "lambda-stage" {
 }
 
 resource "aws_apigatewayv2_integration" "intergation-bw-lambda-and-api-gw" {
-  api_id             = aws_apigatewayv2_api.lambda-api-gw.id
-  integration_type   = "AWS_PROXY"
-  integration_method = "POST"
-  integration_uri    = aws_lambda_function.lambda.invoke_arn
+  api_id               = aws_apigatewayv2_api.lambda-api-gw.id
+  integration_type     = "AWS_PROXY"
+  integration_method   = "POST"
+  integration_uri      = aws_lambda_function.lambda.invoke_arn
   passthrough_behavior = "WHEN_NO_MATCH"
 }
 
 resource "aws_apigatewayv2_route" "lambda_route" {
-  route_key     = "ANY /{proxy+}"
-  target        = "integrations/${aws_apigatewayv2_integration.intergation-bw-lambda-and-api-gw.id}"
-  api_id        = aws_apigatewayv2_api.lambda-api-gw.id
+  route_key = "ANY /{proxy+}"
+  target    = "integrations/${aws_apigatewayv2_integration.intergation-bw-lambda-and-api-gw.id}"
+  api_id    = aws_apigatewayv2_api.lambda-api-gw.id
 }
 
 resource "aws_lambda_permission" "api-gw" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.lambda.arn
   principal     = "apigateway.amazonaws.com"
-  statement_id = "AllowExecutionFromAPIGateway"
-  source_arn = "${aws_apigatewayv2_api.lambda-api-gw.execution_arn}/*/*/*"
+  statement_id  = "AllowExecutionFromAPIGateway"
+  source_arn    = "${aws_apigatewayv2_api.lambda-api-gw.execution_arn}/*/*/*"
 }
